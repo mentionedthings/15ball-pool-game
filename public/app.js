f64ab0a9b47c106d2816dc1273fdfeecdf730eab
mentionedthings <41516850+mentionedthings@users.noreply.github.com>
2026-06-29 23:52:31 +0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io();
+const socket = io(window.location.origin, { transports: ['websocket', 'polling'] });
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const resetBtn = document.getElementById('resetBtn');
